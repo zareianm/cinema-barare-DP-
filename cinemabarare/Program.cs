c2863d9e064b4ba9f628d48a876c181c76f4a0fe
sadegh zareian <m_zareian@comp.iust.ac.ir>
2023-03-10 23:25:07 +0330

    completed

---
+++ cinemabarare/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+
+        int n = int.Parse(s[0]);
+        int B = int.Parse(s[1]);
+
+        s = Console.ReadLine().Split(' ');
+
+        int[] moneys = new int[2 * B];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            moneys[i] = int.Parse(s[i]);
+        }
+        Console.WriteLine(MinCost(n, B, moneys));
+
+    }
+
+    private static long MinCost(int n, int B, int[] moneys)
+    {
+        long[,,] DP = new long[2, B + 1, B + 1];
+        DP[0, 0, 0] = long.MaxValue;
+        DP[1, 0, 0] = long.MaxValue;
+
+
+        for (int i = 0; i <= B; i++)
+        {
+            DP[0, 0, i] = long.MaxValue;
+            DP[1, i, 0] = long.MaxValue;
+
+        }
+        for (int i = 1; i <= B; i++)
+        {
+            for (int j = 1; j <= B; j++)
+            {
+                DP[0, i, j] = Math.Min(DP[0, i - 1, j], DP[1, i - 1, j] + moneys[i + j - 1]);
+                DP[1, i, j] = Math.Min(DP[1, i, j-1], DP[0, i, j - 1] + moneys[i + j - 1]);
+            }
+        }
+        return Math.Min(DP[0, B, B], DP[1, B, B]);
+    }
+}
+
